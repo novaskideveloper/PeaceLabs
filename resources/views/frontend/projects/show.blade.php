7af15930deb73e81f5ae7c525148d55b19f0fe0d
novaskideveloper <novaski93@gmail.com>
2015-12-12 12:31:44 -0200

    facebook

--- resources/views/frontend/projects/show.blade.php
+++ resources/views/frontend/projects/show.blade.php
@@ -45,7 +45,14 @@
       <!-- /KPIS -->
     </section>
     <!-- /HEADER PROJECT PROFILE -->
-
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.5";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 
     <!-- PROJECT DESCRIPTION -->
     <section class="project-description">
